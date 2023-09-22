5e56023afa5d61ac68400572efc70a92effb0186
Bacowskyy <arturmnoyan2004@gmail.com>
2023-09-22 12:16:45 +0200

    22.09.2023

---
+++ console.js
@@ -0,0 +1,10 @@
+console.log("error", "404")
+console.log(window)
+
+console.table(["jeden", "dwa", "trzy"])
+
+console.groupCollapsed("bulbulator")
+console.log("a")
+console.log("b")
+console.log("c")
+console.groupEnd()
